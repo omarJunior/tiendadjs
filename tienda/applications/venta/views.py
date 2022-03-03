eced20a841b76c37c54308bb726ded27b6837380
Omar junior <omarhernandez041204@gmail.com>
2022-03-03 10:50:26 -0500

    views de venta

--- tienda/applications/venta/views.py
+++ tienda/applications/venta/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from .models import Sale, SaleDetail
+from .serializers import VentaReporteSerializer, DetalleVentaProductoSerializer
+
 
 # Create your views here.
+class ReporteVentaList(ListAPIView):
+	serializer_class = VentaReporteSerializer
+
+	def get_queryset(self):
+		venta = Sale.objects.all()
+		return venta
+
+
+class DetailVentaList(ListAPIView):
+	serializer_class = DetalleVentaProductoSerializer
+
+	def get_queryset(self):
+		detalle = SaleDetail.objects.all()
+		return detalle
+
+
+
