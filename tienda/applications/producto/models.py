73d73124eca73561986b42a1063f20ae8ea7e9db
Omar junior <omarhernandez041204@gmail.com>
2022-03-03 10:47:05 -0500

    cambios en la estructura del modelo de app producto

--- tienda/applications/producto/models.py
+++ tienda/applications/producto/models.py
@@ -9,12 +9,7 @@ from .managers import ProductManager
 class Colors(models.Model):
     """ Representa color de un producto """
 
-    color = models.CharField(
-        'Tag', 
-        max_length=120, 
-        unique=True
-    )
-    #
+    color = models.CharField('Tag', max_length=120, unique=True)
 
     class Meta:
         verbose_name = 'Color Producto'
@@ -27,42 +22,14 @@ class Colors(models.Model):
 class Product(TimeStampedModel):
     """Modelo que representa a un producto de tienda"""
 
-    name = models.CharField(
-        'Nombre', 
-        max_length=100
-    )
-    description = models.TextField(
-        'Descripcion producto',
-        blank=True
-    )
-    man = models.BooleanField(
-        'Para Hombre', 
-        default=True
-    ) # es solo para mujer 
-    woman = models.BooleanField(
-        'Para Mujer', 
-        default=True
-    ) # es para varon
-    weight = models.DecimalField(
-        'Peso', 
-        max_digits=5, 
-        decimal_places=2, 
-        default=1
-    )
-    price_purchase = models.DecimalField(
-        'Precio de Compra',
-        max_digits=10,
-        decimal_places=3
-    )
-    price_sale = models.DecimalField(
-        'Precio de Venta',
-        max_digits=10,
-        decimal_places=2
-    )
-    main_image = models.ImageField(
-        'imagen principal',
-        upload_to='producto',
-    ) # imagen principal del producto
+    name = models.CharField('Nombre', max_length=100)
+    description = models.TextField('Descripcion producto',blank=True)
+    man = models.BooleanField('Para Hombre', default=True)
+    woman = models.BooleanField('Para Mujer', default=True)
+    weight = models.DecimalField('Peso', max_digits=5, decimal_places=2, default=1)
+    price_purchase = models.DecimalField('Precio de Compra',max_digits=10,decimal_places=3)
+    price_sale = models.DecimalField('Precio de Venta', max_digits=10, decimal_places=2)
+    main_image = models.ImageField('imagen principal', upload_to='producto') # imagen principal del producto
     image1 = models.ImageField('Imagen 1', blank=True, null=True, upload_to='producto')
     image2 = models.ImageField('Imagen 2', blank=True, null=True, upload_to='producto')
     image3 = models.ImageField('Imagen 3', blank=True, null=True, upload_to='producto')
@@ -71,11 +38,7 @@ class Product(TimeStampedModel):
     video = models.URLField('unboxin', blank=True, null=True)
     stok = models.PositiveIntegerField('Stok', default=0)
     num_sales = models.PositiveIntegerField('Veces vendido', default=0)
-    user_created = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name="prod_created",
-    )
+    user_created = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="prod_created")
 
     objects = ProductManager()
 
