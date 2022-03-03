cb0a8be29f5986bad7eb9fa3b2361ad320376da1
Omar junior <omarhernandez041204@gmail.com>
2022-03-03 10:49:25 -0500

    serializers de la app venta

---
+++ tienda/applications/venta/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from .models import Sale, SaleDetail
+
+class VentaReporteSerializer(serializers.ModelSerializer):
+	productos = serializers.SerializerMethodField(method_name="get_productos")
+
+	class Meta:
+		model = Sale
+		fields = ['id',
+			'date_sale', 
+			'amount', 
+			'count', 
+			'type_invoce', 
+			'cancelado', 
+			'type_payment', 
+			'state' , 
+			'adreese_send', 
+			'anulate', 
+			'user',
+			'productos'
+			]
+
+	def get_productos(self, obj):
+		query = SaleDetail.objects.productos_por_venta(obj.id)
+		productos_serializado = DetalleVentaProductoSerializer(query, many=True).data
+		return productos_serializado
+
+
+class DetalleVentaProductoSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = SaleDetail
+		fields = ['id',
+			'sale',
+			'product',
+			'count',
+			'price_purchase',
+			'price_sale',
+			'anulate'
+		]
