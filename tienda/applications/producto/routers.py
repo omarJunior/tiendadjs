400f9a9fa6257d609abce161c2d98e9f48ce5d29
Omar junior <omarhernandez041204@gmail.com>
2022-03-04 17:41:30 -0500

    router de producto

--- tienda/applications/producto/routers.py
+++ tienda/applications/producto/routers.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .viewsets import ColorsViewSet
+from .viewsets import ColorsViewSet, ProductViewSet
 
 router = DefaultRouter()
 router.register(r"colors", ColorsViewSet, basename="colors")
+router.register(r"product", ProductViewSet, basename="product")
 
 urlpatterns = router.urls
