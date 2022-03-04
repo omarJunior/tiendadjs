baf2773193ddc7107b6f7a10bc5d4ec99dab01d6
Omar junior <omarhernandez041204@gmail.com>
2022-03-04 09:56:34 -0500

    url de tienda

--- tienda/applications/venta/urls.py
+++ tienda/applications/venta/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 
-from .views import ReporteVentaList, DetailVentaList
+from .views import (
+	ReporteVentaList, 
+	DetailVentaList, 
+	RegistrarVenta,
+	RegistrarVenta2,
+)
 
 app_name = "venta_app"
 
 urlpatterns = [
-	path('api/venta/reporte/', ReporteVentaList.as_view()),
-	path('api/venta/detail/', DetailVentaList.as_view()),
-
+	path('api/venta/reporte/', ReporteVentaList.as_view(), name="venta-reporte"),
+	path('api/venta/detail/', DetailVentaList.as_view(), name="venta-detail"),
+	path('api/venta/create/', RegistrarVenta.as_view(), name="venta-register"),
+	path('api/venta/add/', RegistrarVenta.as_view(), name="venta-add"),
 ]
