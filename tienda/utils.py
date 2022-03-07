aac4df221174b6b5da2be23371400760dfef1c6b
Omar junior <omarhernandez041204@gmail.com>
2022-03-07 15:28:22 -0500

    utils

---
+++ tienda/utils.py
@@ -0,0 +1,24 @@
+GENDER_CHOICES = (
+    ('M', 'Masculino'),
+    ('F', 'Femenino'),
+    ('O', 'Otros'),
+)
+
+TIPO_INVOCE = (
+    ('0', 'BOLETA'),
+    ('3', 'FACTURA'),
+    ('4', 'OTRO'),
+)
+
+TIPO_PAYMENT = (
+    ('0', 'TARJETA'),
+    ('1', 'DEPOSITO'),
+    ('2', 'CONTRAENTREGA'),
+)
+
+FLAT_STATE = (
+    ('0', 'En Proceso'),
+    ('1', 'En Envio'),
+    ('2', 'En Tienda'),
+    ('3', 'Entregado'),
+)
